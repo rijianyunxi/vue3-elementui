0ec1f0c761c95d4083638945250f000e3ee0a8e2
song <song@songdeMBP.mshome.net>
2021-07-08 11:35:58 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+let store =  {
   state: {
   },
   mutations: {
@@ -9,4 +9,5 @@ export default createStore({
   },
   modules: {
   }
-})
+}
+export default createStore(store)
