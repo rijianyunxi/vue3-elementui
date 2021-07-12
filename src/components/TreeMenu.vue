c9a6a769dad5d47f3b4da7d3502d08e9e968b122
rijianyunxi <mr.songjintao@gmail.com>
2021-07-12 18:29:35 +0800

    feat: echarts

--- src/components/TreeMenu.vue
+++ src/components/TreeMenu.vue
@@ -37,10 +37,6 @@
       <i class="el-icon-document"></i>
       <template #title>导航三</template>
     </el-menu-item>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <template #title>导航四</template>
-    </el-menu-item>
   </el-menu>
 </template>
 
